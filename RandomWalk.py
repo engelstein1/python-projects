f9f1eab9c3787b46658c21a6b357e2d72a90b352
mefathim <engelstein1>
2023-09-13 09:46:18 +0300

    first

---
+++ RandomWalk.py
@@ -0,0 +1,31 @@
+import random
+import matplotlib.pyplot as plt
+
+
+steps = int(input("please choose an amount of steps: "))
+dims = int(input("please choose a number of possible dimensions: "))
+coordinate_system = [[0 for step in range(steps)] for dim in range(dims)]
+
+def walk():
+    pass
+    # calls move and stay_put
+
+def move(dims, steps):
+    direction = 1
+    chdim = random.randint(0, dims)
+    coin_flip = random.random()
+    if coin_flip < 0.5:
+        direction = -1
+        coordinate_system[chdim][steps +1drunken _
+
+
+
+
+def stay_put(dims, steps):
+    pass
+
+def diplay():
+    pass
+
+walk()
+diplay()
